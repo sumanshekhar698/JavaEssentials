c46c0a68919d9b5e6d1a0c0aa64fcfac7c22a132
Suman Shekhar <suman.sumanshekhar698@gmail.com>
2021-04-18 10:13:48 +0530

    first commit for Java essentials

---
+++ src/com/core/enum_demo/MainFish.java
@@ -0,0 +1,10 @@
+package com.core.enum_demo;
+
+public enum MainFish {
+	Shark , StarFish , Dolphin;
+//	;
+	public static void main(String[] args) {
+		System.out.println("Enum Fish main");	
+	}
+//	Shark , StarFish , Dolphin; // illegal
+}
